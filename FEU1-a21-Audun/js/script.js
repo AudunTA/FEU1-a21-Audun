60d18ded0ad4d29224b94e05e845a2ce85709013
AudunTA <91118159+AudunTA@users.noreply.github.com>
2022-01-10 14:57:12 +0100

    Add files via upload

---
+++ FEU1-a21-Audun/js/script.js
@@ -0,0 +1,41 @@
+//question1
+var navn;
+var booleanValue;
+var age;
+
+navn = "Audun";
+booleanValue = true;
+age = 24;
+
+console.log(navn +" "+ booleanValue +" "+ age);
+
+//question 2
+var firstName = "Audun";
+var lastName = "Anderssen";
+
+var fullName = firstName+" "+lastName;
+console.log(fullName);
+
+
+
+//question 3
+var animal ="Frog";
+
+typeOfFrog= typeof animal;
+
+console.log("The type of frog is " + typeOfFrog);
+
+//question 4
+var orderHasShipped = true;
+if(orderHasShipped) {
+    console.log("order has shipped");
+}
+else{
+    console.log("order has not shipped");
+}
+
+
+//console log the number 7-13
+for(var i = 7; i <=13; i++) {
+    console.log(i);
+}
